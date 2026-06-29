e6c55300bfec26a8865870f7a3287d80aaeb6695
AhmadAlyashoty <90099990ahmadzaaza@gmail.com>
2026-06-29 18:05:45 +0200

    upload dataset loader

---
+++ data/load_dataset.py
@@ -0,0 +1,16 @@
+from datasets import load_dataset
+import pandas as pd
+
+
+def load_cnn_dataset():
+    dataset = load_dataset("cnn_dailymail", "3.0.0")
+    return dataset
+
+def save_to_csv(dataset, filename):
+    df = pd.DataFrame(dataset['train'][:3000]) 
+    df.to_csv(filename, index=False)
+    return df
+
+dataset = load_cnn_dataset()
+df = save_to_csv(dataset, 'my_training_data.csv')
+print(df.head)
